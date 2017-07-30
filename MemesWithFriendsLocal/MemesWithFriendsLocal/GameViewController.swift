ab15998843a5929286229f9083124a442723c691
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-07-30 16:34:38 -0600

    Add Alert's to voting system

--- MemesWithFriendsLocal/MemesWithFriendsLocal/GameViewController.swift
+++ MemesWithFriendsLocal/MemesWithFriendsLocal/GameViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class GameViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    //MARK: - UICollection View DataSource / Delegate 
+    
+    //MARK: - UICollection View DataSource / Delegate
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return MemeController.shared.memes.count
@@ -28,10 +28,10 @@ class GameViewController: UIViewController, UICollectionViewDelegate, UICollecti
         cell.topTextLabel.text = meme.firstText
         cell.bottomTextLabel.text = meme.secondText
         cell.memeImage.image = meme.image
-
+        
         return cell
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let meme = MemeController.shared.memes[indexPath.row]
         meme.voteCount += 1
@@ -40,17 +40,23 @@ class GameViewController: UIViewController, UICollectionViewDelegate, UICollecti
         if GameController.shared.game?.numberOfVotes == 0 {
             NSLog("Numbers of votes 0")
             performSegue(withIdentifier: "resultsSegue", sender: self)
+        } else {
+            let alert = UIAlertController(title: "Next Person", message: "Please pass device to the next person", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true)
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
