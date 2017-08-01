3eb7f20f54f404bc4f275ddba1556aadc7ab233c
Zac Duvall <duvallzac@gmail.com>
2017-08-01 11:18:36 -0600

    Update UI / Font

--- MemesWithFriendsLocal/MemesWithFriendsLocal/MainViewController.swift
+++ MemesWithFriendsLocal/MemesWithFriendsLocal/MainViewController.swift
@@ -10,7 +10,43 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var playButton: AppButton!
+    @IBOutlet weak var instructionsButton: AppButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        logoImageView.alpha = 0
+        playButton.alpha = 0
+        instructionsButton.alpha = 0
+        showLogo()
     }
+    
+    
+    //MARK: - Methods
+    func showLogo() {
+        
+        UIView.animate(withDuration: 2, delay: 0.0, options: [.transitionCrossDissolve], animations: {
+            self.logoImageView.alpha = 1
+        }) { (true) in
+            self.showPlayButton()
+        }
+    }
+    
+    func showPlayButton() {
+        UIView.animate(withDuration: 1, delay: 0.0, options: .transitionCrossDissolve, animations: { 
+            self.playButton.alpha = 1
+        }) { (true) in
+            self.showInstructionsButton()
+        }
+    }
+    
+    func showInstructionsButton() {
+        UIView.animate(withDuration: 1, delay: 0.0, options: .transitionCrossDissolve, animations: { 
+            self.instructionsButton.alpha = 1
+        }, completion: nil)
+    }
+    
 }
