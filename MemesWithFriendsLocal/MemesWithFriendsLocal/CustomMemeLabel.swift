61f99c1e065cdfdc674bee503833304cb0a291be
Zac Duvall <duvallzac@gmail.com>
2017-08-01 09:39:22 -0600

    Add Custom 'Impact' Font, Update label sizing on Memes

---
+++ MemesWithFriendsLocal/MemesWithFriendsLocal/CustomMemeLabel.swift
@@ -0,0 +1,38 @@
+//
+//  CustomMemeLabel.swift
+//  MemesWithFriendsLocal
+//
+//  Created by Zachary Duvall on 8/1/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomMemeLabel: UILabel {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var rotationAngle: CGFloat = 0 {
+        didSet {
+            self.transform = CGAffineTransform(rotationAngle: rotationAngle * .pi / 180)
+        }
+    }
+
+}
