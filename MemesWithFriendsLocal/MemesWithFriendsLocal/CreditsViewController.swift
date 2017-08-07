a630815cb00620a91a75df08352a22f15c68e062
Zac Duvall <duvallzac@gmail.com>
2017-08-07 16:57:19 -0600

    Implement 'About' Screen

---
+++ MemesWithFriendsLocal/MemesWithFriendsLocal/CreditsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreditsViewController.swift
+//  MemesWithFriendsLocal
+//
+//  Created by Zachary Duvall on 8/7/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
